bfee6140a1071045c7d855cab6cf87afdd0633b3
dipu-m18 <dipannitamahata@gmail.com>
2021-02-07 23:39:35 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from "./admin/admin-home/admin-home.component";
 import { AdminRegistrationComponent } from './admin/admin-landing-page/admin-registration/admin-registration.component';
 import { AdminLoginComponent } from './admin/admin-landing-page/admin-login/admin-login.component';
 import { ImagesComponent } from './images/images.component';
@@ -7,6 +8,7 @@ import { ImageComponent } from './images/image/image.component';
 import { ImageListComponent } from './images/image-list/image-list.component';
 
 const routes: Routes = [
+  {path: 'admin/home', component: AdminHomeComponent},
   {path: 'admin/register', component: AdminRegistrationComponent},
   {path: 'admin/login', component: AdminLoginComponent},
   {
